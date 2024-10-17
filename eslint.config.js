6fe6cb8813d5a208c5e0e6d09d6d19b0b7925db4
George Perry <gperry@exploratorium.edu>
2024-10-16 22:32:28 -0700

    must import jest

--- eslint.config.js
+++ eslint.config.js
@@ -55,6 +55,7 @@ export default [
       globals: {
         ...eslintPluginJest.environments.globals.globals,
         ...globals.jest,
+        jest: 'off',
       },
     },
   },
--- src/index.test.js
+++ src/index.test.js
@@ -1,3 +1,5 @@
+import { jest } from '@jest/globals';
+
 import drupalSdcGenerator from './index.js';
 
 describe('drupalSdcGenerator', () => {
