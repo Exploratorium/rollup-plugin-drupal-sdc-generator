7a844558cc4b6b77525d58482ac4768c9e9a9d68
George Perry <gperry@exploratorium.edu>
2024-10-16 22:22:07 -0700

    updated eslint

---
+++ eslint.config.js
@@ -0,0 +1,61 @@
+import pluginJs from '@eslint/js';
+import prettier from 'eslint-config-prettier';
+import eslintPluginJest from 'eslint-plugin-jest';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import eslintPluginSimpleImportSort from 'eslint-plugin-simple-import-sort';
+import eslintPluginUnusedImports from 'eslint-plugin-unused-imports';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: [
+      'examples/*/components',
+      'examples/*/js',
+      'examples/**/*/public',
+      'dist',
+      'node_modules',
+      'package-lock.json',
+    ],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  prettier,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        Drupal: 'readonly',
+      },
+    },
+    plugins: {
+      prettier: eslintPluginPrettier,
+      'simple-import-sort': eslintPluginSimpleImportSort,
+      'unused-imports': eslintPluginUnusedImports,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'simple-import-sort/imports': 'error',
+      'unused-imports/no-unused-imports': 'error',
+    },
+  },
+  {
+    files: ['src/**/*.test.js'],
+    plugins: {
+      jest: eslintPluginJest,
+    },
+    languageOptions: {
+      globals: {
+        ...eslintPluginJest.environments.globals.globals,
+        ...globals.jest,
+      },
+    },
+  },
+];
